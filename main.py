0a67d60b70f0a0b1dd3e95c6dd9f8338f3bcd469
slakh96 <s.lakhani1996@gmail.com>
2020-02-16 08:55:35 -0500

    Added function to write transaction data to JSON file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for, render_template, request, Response, redirect, jsonify
 from flask_static_compress import FlaskStaticCompress
+from typing import Dict, List
+import json
 
 app = Flask(__name__)
 app.config['COMPRESSOR_DEBUG'] = app.config.get('DEBUG')
@@ -9,17 +11,50 @@ app.static_folder = 'static'
 compress = FlaskStaticCompress(app)
 
 
-
 @app.route('/', methods=['GET'])
 def home():
     """Landing page."""
     print("Reached base redirect")
     return redirect(url_for('static', filename='index.html'))
 
+
 @app.route('/launchDivi', methods=['GET'])
 def launchDivi():
     return redirect(url_for('static', filename='chatbot.html'))
 
 
+def readTransactionData(fileName: str) -> List[Dict]:
+    """Given the complete name of a JSON file such as johnsTransactions.json, this function finds the file and reads it
+    in to a list of dictionary objecst and returns it, e.g.
+    [{'time': '2020-01-31T23:48:36.969Z', 'value': 99.73, 'cardNum': 234567890, 'bank': 'RBC', 'merchant': 'No Frills',
+     'description': 'Food'}]"""
+    try:
+        with open("./static/data/" + fileName, 'r') as f:
+            json_text = f.read()
+    except:
+        print("There was an invalid file name provided.")
+        return []
+    transactions = json.loads(json_text)
+    #print(transactions)
+    #print(type(transactions))
+    return transactions
+
+
+def writeTransactionData(fileName: str, data: List ) -> None:
+    """Given the completes"""
+    jsonfied = json.dumps(data)
+    print(jsonfied)
+
+    try:
+        with open("./static/data/" + fileName, 'w') as f:
+            print("Open success")
+            f.write(jsonfied)
+    except:
+        print("There was an invalid file name provided.")
+        return
+
+
 if __name__ == "__main__":
+    existingTransactions = readTransactionData("johnsTransactions.json")
+    writeTransactionData("junweisTransactions.json", existingTransactions)
     app.run(debug=True)
